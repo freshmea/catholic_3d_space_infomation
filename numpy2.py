a0ec46fd473177f1474c21e5aaf37db258e8539c
freshmea <fresmea@naver.com>
2023-08-22 00:30:26 -0700

    aa

--- numpy2.py
+++ numpy2.py
@@ -6,8 +6,10 @@ def show(m, o):
 def main():
     array1 = np.arange(27, dtype=np.int8)
     show('array1', array1)
-    array2 = np.linspace(1, 10, 19)
+    array2 = np.arange(1, 10+1, 0.1)
     show('array2', array2)
+    array3 = np.linspace(1, 10, 19)
+    show('array3', array3)
 
 if __name__ == '__main__':
     main()
